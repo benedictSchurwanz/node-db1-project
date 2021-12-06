bb04f374157ac94e0d6b3d713139b3785386a092
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-06 16:48:28 -0600

    MVP queries completed

--- queries.sql
+++ queries.sql
@@ -13,8 +13,15 @@ select * from orders order by orderdate desc limit 10;
 select * from customers where city like 'london' or city like 'madrid' or city like 'brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into customers (customername, contactname, address, city, postalcode, country) 
+values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle-Earth');
+-- though I believe it should be as follows: 
+-- insert into customers (customername, contactname, address, city, postalcode, country) values ('The Shire', 'Bilbo Baggins', 'Bag End', 'Hobbiton', '111', 'The Shire');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+update customers 
+set postalcode='11122'
+where customername like 'The Shire';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
